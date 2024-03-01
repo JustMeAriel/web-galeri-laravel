cce1bb88a47851245c6f19992bd960827cf34d29
JustMeAriel <arieleka058@gmail.com>
2024-03-01 23:34:03 +0700

    renew

--- database/migrations/2024_02_04_111459_create_galeri.php
+++ database/migrations/2024_02_04_111459_create_galeri.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_galeri', function (Blueprint $table) {
+        Schema::create('galeri', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('content');
